8a5e5cee2fdf69f29979caf1c6df5110751c1201
csestelo <sestelos@gmail.com>
2018-12-02 18:10:42 -0200

    seta cor em logo svg, alinha barras de progresso na esquerda e mostra icone de check caso a task tenha sido finalizada

--- src/App.js
+++ src/App.js
@@ -1,36 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import finished from './finished.svg';
 import './App.css';
 
 
 class ProgressBar extends Component {
     constructor() {
-        super();
+        super(ProgressBar);
         this.state = {
             start: null,
             finish: null,
-            totalSteps: 100,
-            currentStep: 70
+            totalSteps: 8,
+            currentStep: 8
         }
     }
 
-    getCurrentPercentage(){
-        return "20%"
+    getCurrentPercentage() {
+        if (this.state.currentStep === null) {
+            return "0%"
+        } else {
+            const current_pct = this.state.currentStep / this.state.totalSteps * 100;
+            return `${current_pct.toString()}%`
+        }
     }
 
     isFinished(){
-        return this.state.currentStep === this.state.totalSteps;
-    }
+        return this.state.currentStep === this.state.totalSteps
+    };
+
+    displayIsFinished() {
+        if (this.isFinished()) {
+            return "block"
+        } else {
+            return "none"
+        }
+    };
 
     update(step){
     }
 
     render() {
         return (
-            <div>
-                <p>{this.props.taskName}</p>
+            <div className="singleTask">
+                <p className="taskName">{this.props.taskName}:</p>
                 <div className="progress-bar">
                     <div className="progress" style={{width: this.getCurrentPercentage()}}>
+                        <div style={{display: this.displayIsFinished()}}>
+                            <img src={finished} className="checkIsFinished" alt="checkLogo" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -43,7 +59,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
             <ProgressBar taskName={"barra1"} />
             <ProgressBar taskName={"barra2"} />
             <ProgressBar taskName={"barra3"} />
