0e3dee501b426558e4849e3afcf90c735e7c4046
csestelo <sestelos@gmail.com>
2018-12-10 00:07:31 -0200

    mocka estimulos externos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import finished from './finished.svg';
 import './App.css';
 
 
+const randomstring = require("randomstring");
+
 class ProgressBar extends Component {
     constructor(props) {
         super(props);
@@ -10,12 +12,12 @@ class ProgressBar extends Component {
             start: new Date(),
             finish: null,
             totalSteps: props.totalSteps,
-            currentStep: null
+            currentStep: props.currentStep
         }
     }
 
     getCurrentPercentage() {
-        if (this.state.currentStep === null) {
+        if (this.state.currentStep === 0) {
             return "0%"
         } else {
             const current_pct = this.state.currentStep / this.state.totalSteps * 100;
@@ -62,16 +64,55 @@ class ProgressBar extends Component {
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {tasks: ["barra1", "barra2", "barra3"]}
+        this.state = {tasks: []}
     };
 
     // TODO: criar um setInterval que a cada 10 segundos cria uma nova task
     // TODO: criar um setInterval que a cada 2 segundos incrementa aleatoriamente
     // o step de uma task existente
 
+    getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    componentDidMount() {
+        setInterval(
+            () => {
+                const newTask = randomstring.generate({
+                    length: 10,
+                    charset: 'alphabetic'
+                });
+
+                let curr_tasks = this.state.tasks;
+                curr_tasks.push({
+                    name: newTask,
+                    totalSteps: this.getRandomInt(3, 10),
+                    currentStep: 0});
+                this.setState({tasks: curr_tasks})
+            },
+            5000);
+        setInterval(
+            () => {
+                if (this.state.tasks.length) {
+                    const tasks = this.state.tasks;
+                    let i = this.getRandomInt(0, tasks.length - 1);
+                    let task = tasks[i];
+                    task.currentStep += 1;
+                    tasks[i] = task;
+
+                    this.setState({tasks: tasks})
+                }
+            },
+            2000
+        )
+    }
+
     render() {
         const progressBars = this.state.tasks.map(function (value, index) {
-            return <ProgressBar key={index} taskName={value} totalSteps={8}/>
+            console.log(value.currentStep);
+            return <ProgressBar key={index} taskName={value.name} totalSteps={value.totalSteps} currentStep={value.currentStep}/>
         });
 
         return (
